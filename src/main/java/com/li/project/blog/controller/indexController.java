f3f55bac7e38bc46bba7b39ef97cdb363d434764
LiHeQian6 <1142808063@qq.com>
2020-02-07 16:37:21 +0800

    创建仓库
    Signed-off-by: LiHeQian6 <1142808063@qq.com>

---
+++ src/main/java/com/li/project/blog/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.li.project.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping("/admin")
+    public String index(){
+        int a=9/0;
+        return "management";
+    }
+}
---
+++ src/main/java/com/li/project/blog/handler/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.li.project.blog.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    Logger logger= LoggerFactory.getLogger(getClass());
+
+    //@ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(HttpServletRequest request, Exception e){
+        logger.error("Request url: {} Exception: {}",request.getRequestURI(),e);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url",request.getRequestURI());
+        modelAndView.addObject("exception",e);
+        modelAndView.setViewName("error/error");
+        return modelAndView;
+    }
+}
